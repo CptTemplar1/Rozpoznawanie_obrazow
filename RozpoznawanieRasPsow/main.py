69cddc8e4fb32256c7de3f4f6a1c1aea468c26d1
CptTemplar1 <michal2001k@op.pl>
2023-10-18 16:39:04 +0200

    Prosta aplikacja Kivy wyświetlająca HelloWorld w różnych kolorach

--- RozpoznawanieRasPsow/main.py
+++ RozpoznawanieRasPsow/main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.utils import get_color_from_hex
 
 
-# Press the green button in the gutter to run the script.
+class HelloWorldApp(App):
+    def build(self):
+        self.colors = [
+            '#FF5733',  # Czerwony
+            '#33FF57',  # Zielony
+            '#3377FF',  # Niebieski
+        ]
+        self.current_color_index = 0
+
+        self.layout = BoxLayout(orientation='vertical')
+        self.label = Label(text="Hello, World!", font_size='30sp', color=get_color_from_hex(self.colors[0]))
+        self.button = Button(text="Zmień kolor", on_press=self.change_color)
+        self.layout.add_widget(self.label)
+        self.layout.add_widget(self.button)
+        return self.layout
+
+    def change_color(self, instance):
+        self.current_color_index = (self.current_color_index + 1) % len(self.colors)
+        self.label.color = get_color_from_hex(self.colors[self.current_color_index])
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    HelloWorldApp().run()
