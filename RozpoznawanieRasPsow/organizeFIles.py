f131cf6144ecea5f4ce6f51aaa808c9600817f3f
CptTemplar1 <michal2001k@op.pl>
2023-11-07 13:52:18 +0100

    Dodano skrypt organizacji plików do uczenia sieci

---
+++ RozpoznawanieRasPsow/organizeFIles.py
@@ -0,0 +1,66 @@
+import collections
+import math
+import os
+import shutil
+
+from d2l import torch as d2l
+
+#@save
+def read_csv_labels(fname):
+    """Read `fname` to return a filename to label dictionary."""
+    with open(fname, 'r') as f:
+        # Skip the file header line (column name)
+        lines = f.readlines()[1:]
+    tokens = [l.rstrip().split(',') for l in lines]
+    return dict(((name, label) for name, label in tokens))
+
+#@save
+def copyfile(filename, target_dir):
+    """Copy a file into a target directory."""
+    os.makedirs(target_dir, exist_ok=True)
+    shutil.copy(filename, target_dir)
+
+#@save
+def reorg_train_valid(data_dir, labels, valid_ratio):
+    """Split the validation set out of the original training set."""
+    # The number of examples of the class that has the fewest examples in the
+    # training dataset
+    n = collections.Counter(labels.values()).most_common()[-1][1]
+    # The number of examples per class for the validation set
+    n_valid_per_label = max(1, math.floor(n * valid_ratio))
+    label_count = {}
+    for train_file in os.listdir(os.path.join(data_dir, 'train')):
+        label = labels[train_file.split('.')[0]]
+        fname = os.path.join(data_dir, 'train', train_file)
+        copyfile(fname, os.path.join(data_dir, 'train_valid_test',
+                                     'train_valid', label))
+        if label not in label_count or label_count[label] < n_valid_per_label:
+            copyfile(fname, os.path.join(data_dir, 'train_valid_test',
+                                         'valid', label))
+            label_count[label] = label_count.get(label, 0) + 1
+        else:
+            copyfile(fname, os.path.join(data_dir, 'train_valid_test',
+                                         'train', label))
+    return n_valid_per_label
+
+
+#@save
+def reorg_test(data_dir):
+    """Organize the testing set for data loading during prediction."""
+    for test_file in os.listdir(os.path.join(data_dir, 'test')):
+        copyfile(os.path.join(data_dir, 'test', test_file),
+                os.path.join(data_dir, 'train_valid_test', 'test',
+                             'unknown'))
+
+
+def reorg_dog_data(data_dir, valid_ratio):
+    labels = d2l.read_csv_labels('C:/Users/micha/OneDrive/Pulpit/dog-breed-identification/labels.csv')
+    d2l.reorg_train_valid(data_dir, labels, valid_ratio)
+    d2l.reorg_test(data_dir)
+
+
+
+data_dir = 'C:/Users/micha/OneDrive/Pulpit/dog-breed-identification'
+batch_size = 128
+valid_ratio = 0.1
+reorg_dog_data(data_dir, valid_ratio)
